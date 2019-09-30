d6ca2e363552e3003c915d84026fd3e148bf8319
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-30 04:49:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,12 @@ class Song
   def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create(name)
 end
-def self.new_from_filename(name)
-  Song.
+def self.new_from_filename(file)
+  file = file.gsub(".mp3", "")
+  artist, song, genre = file.split(" - ")
+  song_artist = Artist.find_or_create_by_name(artist)
+  song_genre = Genre.find_or_create_by_name(genre)
+  new_song = Song.new(song, song_artist, song_genre)
 end
+
 end
